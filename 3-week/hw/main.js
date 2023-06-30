039a3d253d0a4e0b5aebaa2f127e6c1a8f5117e1
Miron Khoruzhenko <mironkhoruzhenko@gmail.com>
2023-06-30 21:39:55 +0300

    2023.06.30

--- 3-week/hw/main.js
+++ 3-week/hw/main.js
@@ -4,14 +4,14 @@
 const loader = document.querySelector("#loader");
 const pink = document.querySelector(".pink");
 
-const tween = KUTE.fromTo(
-	'#blob1',
-	{path: '#blob1'},
-	{path: '#blob2'},
-	{repeat: 999, duration: 3000, yoyo: true}
-)
+	const tween = KUTE.fromTo(
+		'#blob1',
+		{path: '#blob1'},
+		{path: '#blob2'},
+		{repeat: 999, duration: 3000, yoyo: true}
+	)
 
-tween.start()
+	tween.start()
 window.addEventListener('load', ()=>{
 	setTimeout(()=>{
 		loader.classList.add('hidden')
